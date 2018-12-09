708e5225bbcc233e14d4b87cae548f70ed9624c9
Siyu Zhou <siyu.zhou@outlook.com>
2018-12-09 11:23:24 -0700

    Remove env print screen.

--- project2-continous_control/train.py
+++ project2-continous_control/train.py
@@ -107,18 +107,13 @@ if __name__ == "__main__":
 
     # number of agents
     num_agents = len(env_info.agents)
-    print('Number of agents:', num_agents)
 
     # size of each action
     action_size = brain.vector_action_space_size
-    print('Size of each action:', action_size)
 
     # examine the state space
     states = env_info.vector_observations
     state_size = states.shape[1]
-    print('There are {} agents. Each observes a state with length: {}'.format(
-        states.shape[0], state_size))
-    print('The state for the first agent looks like:', states[0])
 
     # Create agent
     agent = DDPGAgent(state_size=state_size, action_size=action_size, random_seed=10)
